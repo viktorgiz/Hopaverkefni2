6892bd1f50db45ff2f65c9c12232889c030b70d8
viktorgiz <47834212+viktorgiz@users.noreply.github.com>
2019-11-30 00:11:15 +0000

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import List, {empty, fyrirlesturBirta} from './lib/list';
+import List, { empty, fyrirlesturBirta } from './lib/list';
 import toggle from './lib/toggle';
 // import fetchData from './lib/storage';
 import fyrirlestur, { klaraFyrirlestur, FaraAForsidu } from './lib/fyrirlestur';
@@ -6,27 +6,21 @@ import fyrirlestur, { klaraFyrirlestur, FaraAForsidu } from './lib/fyrirlestur';
 let jsonList;
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.load = function() {
-    this.localStorage.clear;
-  }
-
-
   const page = document.querySelector('body');
   const isLecturePage = page.classList.contains('lecture-page');
 
   if (isLecturePage) {
     fyrirlestur();
-    document.addEventListener('click', ()=>{
+    document.addEventListener('click', () => {
       klaraFyrirlestur(event);
       FaraAForsidu(event);
-      
-    })
+    });
   } else {
     const list = new List();
 
     jsonList = JSON.parse(localStorage.getItem('json-file'));
- 
-    document.addEventListener('click', ()=>{
+
+    document.addEventListener('click', () => {
       toggle(event);
       empty(list.container);
       list.upploadClasses(jsonList);
